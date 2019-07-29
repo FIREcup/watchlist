56c8ce1a907635268022934e0e79c359262f7bd1
jackyang <jackyang@linux.com>
2019-07-29 22:45:13 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ import os, sys
 import click
 
 
+movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'}
+]
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭读模型修改的监控
@@ -15,13 +27,23 @@ db = SQLAlchemy(app)
 @app.route("/home")
 @app.route("/")
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    return render_template('index.html', movies=movies)
 
 
 @app.route("/user/<name>")
 def user_page(name):
     return 'User: {}'.format(name)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,6 +54,7 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
+
 
 
 @app.cli.command()

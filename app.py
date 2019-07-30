fab6c0ee46130386ddb260c9ba934bfe5e1495a7
jackyang <jackyang@linux.com>
2019-07-30 23:35:34 +0800

    User authentication with flask-login

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import os, sys
 import click
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 
 movies = [
@@ -22,13 +24,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_pat
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭读模型修改的监控
 app.config['SECRET_KEY'] = "dev"
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
-@app.route("/index")
-@app.route("/home")
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+@app.route('/index', methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = request.form.get('title')
         year = request.form.get('year')
         if not title or not year or len(year) > 4 or len(title) > 60:
@@ -43,12 +53,28 @@ def index():
     return render_template('index.html', movies=movies)
 
 
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def setting():
+    if request.method == 'POST':
+        name = request.form.get('username')
+        if not name or len(name) > 60:
+            flash('Invalid Input.')
+            return redirect(url_for('setting'))
+        current_user.username = name
+        db.session.commit()
+        flash('Settings updated...')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
+
+
 @app.route("/user/<name>")
 def user_page(name):
     return 'User: {}'.format(name)
 
 
 @app.route("/movie/edit/<int:movie_id>", methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -67,11 +93,42 @@ def edit(movie_id):
 
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash('Item deleted.')
+    return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not username or not password:
+            flash('Invalid Input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login Success.')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Good Bye.")
     return redirect(url_for('index'))
 
 
@@ -86,9 +143,17 @@ def inject_user():
     user = User.query.first()
     return dict(user=user)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def generate_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Movie(db.Model):
@@ -135,5 +200,29 @@ def forge():
         movie = Movie(title=m['title'], year=m['year'])
         db.session.add(movie)
 
+    db.session.commit()
+    click.echo('Done.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username to log in.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True,
+        help='The password to log in.')
+def admin(username, password):
+    """
+    create admin.
+    """
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.generate_password(password)
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name="Admin")
+        user.generate_password(password)
+        db.session.add(user)
     db.session.commit()
     click.echo('Done.')
